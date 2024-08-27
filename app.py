608d64bc0ccd25e4131aa98f0dd9bd1a5521d731
Kaushal Vadadoria <150444299+Kaushalvadadoria28@users.noreply.github.com>
2024-08-27 14:05:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
-nltk.download('punkt')
+nltk.download('punkt_tab')
 ps = PorterStemmer()
 
 def transform_text(text):
